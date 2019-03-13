334010583623917b047852b726ef899a11628659
jonathanjustus <47228122+jonathanjustus@users.noreply.github.com>
2019-03-12 21:28:56 -0500

    Clone of lab7 with TODO

---
+++ Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-03-12
+OU ID_______________: 113480781
+OU EMAIL____________: jonathan.justus@ou.edu
+
+Tasks:
+1. sickPerson Class
+2. healthyPerson Class
+3. HosStack
+4. HosQueue
+5. HosPriorityQueue
+6. Editing and Testing
+
+Estimated Time (minutes):
+1. 30
+2. 20
+3. 15
+4. 15
+5. 40
+6. 60
+
+Actual Time (minutes):
+1. 35
+2. 30
+3. 15
+4. 10
+5. 40
+6. 75
+*/}
